759b81ccfba8b5dcbf2018bd119e58b4302bbbae
romario2121 <80700951+romario2121@users.noreply.github.com>
2024-05-24 17:18:57 +0300

    Update Hill.cpp

--- ConsoleApplication4/Hill.cpp
+++ ConsoleApplication4/Hill.cpp
@@ -1,64 +1,153 @@
-
-1)
-• int* pOne;//pOne указатель на значение типа int
-
-• int vTwo;// vTwo является переменной int целое число
-
-• int* pThree = &vTwo;// pThree указатель с переменной int и принемает значение адресса переменной &vTwo
-
-
-2)
-unsigned short yourAge;
-unsigned short *pYourAge = &yourAge;
-
-3)
-*pYourAge = 50;
-
-
-
-4)
 #include <iostream>
+using namespace std;
 
+int main()
+ {
+    int number = 10; 
+    int *pnumber = &number; 
+
+    int &rnumberRef = number; 
+
+    cout << " variable: " << number << endl;
+    cout << " pnumber: " << *pnumber << endl;
+    cout << " rnumberRef: " << rnumberRef << endl;
+
+    *pnumber = 29;
+
+    cout << "variable: " << number << endl;
+    cout << " pnumber: " << *pnumber << endl;
+    cout << " rnumberRef: " << rnumberRef << endl;
+
+ 
+    rnumberRef = 10;
+
+    cout << " variable: " << number << endl;
+    cout << " pnumber: " << *pnumber << endl;
+    cout << " rnumberRef: " << rnumberRef << endl;
+
+    return 0;
+
+}
+
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    const int *constNum = 0; 
+    int varOne = 6; 
+    constNum = &varOne; 
+
+    cout << " varOne: " << varOne << endl;
+    *constNum = 7; // constNum нельзя назначить уже на обьявленный объект
+
+    int varTwo = 10; 
+    constNum = &varTwo; 
+    cout << " varTwo: " << *constNum << endl;
+
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+
+ short *pwanderingPointer = new short;
+*pwanderingPointer = 10;
+cout <<" PwanderingPoint " << *pwanderingPointer <<endl;
+delete pwanderingPointer;
+
+long *plong = new long;
+*plong = 90000;
+cout << "plong " << *plong << endl;
+
+*pwanderingPointer = 20;
+
+cout <<" PwanderingPoint " << *pwanderingPointer <<endl;
+cout <<" plong " << *plong <<endl;
+delete plong;
+
+    return 0;
+}
+
+#include <iostream>
 using namespace std;
 
 int main() {
-    int yourAge;     
-    int *pYourAge = &yourAge; 
+    short *pwanderingPointer = new short;
+    *pwanderingPointer = 10;
+    cout << "PwanderingPoint " << *pwanderingPointer << endl;
+    delete pwanderingPointer;
+    pwanderingPointer = nullptr; 
 
-   
-    *pYourAge = 30;
+    long *plong = new long;
+    *plong = 90000;
+    cout << "plong " << *plong << endl;
 
-    cout << "yourAge: " << yourAge << endl;
-    cout << " pYouAge: " << *pYourAge << endl;
+    if (pwanderingPointer != nullptr)
+     {
+        *pwanderingPointer = 20; 
+        cout << "PwanderingPoint " << *pwanderingPointer << endl;
+    } 
+    else
+     {
+        cout << "PwanderingPointer nullptr" << endl;
+     }
+
+    cout << "plong " << *plong << endl;
+    delete plong;
 
     return 0;
 }
 
-5)
 #include <iostream>
 
-using namespace std;
-
 int main()
-{
-    int *pInt;// не иницилизированая переменная, можно исправить оператором new
-    *pInt = 9;
-    cout << " The value at pInt: " << *pInt;
-    //можно использовать оператор delete для освобождения выделеной памяти переменной pInt
+ {
+    while (true)
+     {
+        int *ptr = new int; 
+       
+       delete ptr;
+     }
+
     return 0;
 }
 
-6)
+
+
+
+
 #include <iostream>
 
 using namespace std;
 
+class Cat
+{
+public:
+    Cat(int age)  {itsAge = age;}
+    ~Cat() {}
+    int GetAge() const { return itsAge; }
+
+private:
+    int itsAge;
+};
+
+Cat &MakeCat(int age);
+
 int main()
 {
-    int SomeVariable = 5;
-    cout << "SomeVariable: " << SomeVariable << "\n";
-    int *pVar = &SomeVariable;
-    *pVar = 9;// отсуствует указатель, для изменения значения с 5 на 9
-    cout << "SomeVariable: " << *pVar << "\n";
+    int age = 7;
+    Cat &Boots = MakeCat(age);// объявляем ссылку Boots которая  указывает на объект тип Cat
+    cout << "Boots is: " << Boots.GetAge() << " years old" << endl;
+    delete &Boots;// освобождение динамической памяти в main для того чтоб не было утечки памяти!
     return 0;
+}
+
+Cat &MakeCat(int age)
+{
+    Cat *pCat = new Cat(age);
+    return *pCat;
 }
